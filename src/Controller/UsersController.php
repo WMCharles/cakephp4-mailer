2571c11b3539cdda52619e89b401690a435ba06c
Charles Wafula <charles.wafula@student.moringaschool.com>
2023-03-31 11:10:40 +0300

    change smtp

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -52,58 +52,6 @@ class UsersController extends AppController
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
      */
-    // public function add()
-    // {
-    //     $user = $this->Users->newEmptyEntity();
-    //     if ($this->request->is('post')) {
-    //         $user = $this->Users->patchEntity($user, $this->request->getData());
-    //         if ($this->Users->save($user)) {
-
-    //             $mail = new PHPMailer(true);
-
-    //             try {
-    //                 //Server settings
-    //                 $mail->isSMTP();                                            //Send using SMTP
-    //                 $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    //                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    //                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    //                 $mail->Username   = 'wafulacharles47@gmail.com';                     //SMTP username
-    //                 $mail->Password   = 'totnzlfqpbyehmyo';                               //SMTP password
-    //                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    //                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //                 //Recipients
-    //                 $mail->setFrom('wafulacharles47@gmail.com', 'Masinde Charles');
-    //                 $mail->addAddress($user->email, $user->name);     //Add a recipient
-
-    //                 // Set email body content from file template
-    //                 $templatePath = ROOT . DS . 'templates' . DS . 'email' . DS . 'html' . DS . 'account_creation.php';
-
-    //                 $emailBodyContent = file_get_contents($templatePath);
-    //                 $emailBodyContent = str_replace('{$user_name}', $user->name, $emailBodyContent);
-    //                 $emailBodyContent = str_replace('{$user_email}', $user->email, $emailBodyContent);
-    //                 $emailBodyContent = str_replace('{$user_password}', $user->password, $emailBodyContent);
-
-    //                 // Set email properties
-    //                 $mail->isHTML(true);                                  //Set email format to HTML
-    //                 $mail->Subject = 'Account Creation';
-    //                 $mail->Body    = $emailBodyContent;
-    //                 $mail->AltBody = 'Success!';
-
-    //                 // Send email
-    //                 $mail->send();
-    //                 $this->Flash->success(__('The user has been saved.'));
-    //                 return $this->redirect(['action' => 'index']);
-    //             } catch (Exception $e) {
-    //                 $errorMessage = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    //                 $this->Flash->error(__('The user has been saved but the email could not be sent. Please try again later. Error message: {0}', $errorMessage));
-    //                 return $this->redirect(['action' => 'index']);
-    //             }
-    //         }
-    //         $this->Flash->error(__('The user could not be saved. Please, try again.'));
-    //     }
-    //     $this->set(compact('user'));
-    // }
     public function add()
     {
         $user = $this->Users->newEmptyEntity();
@@ -115,14 +63,14 @@ class UsersController extends AppController
 
                 try {
                     //Server settings
-                    $mail->isSMTP();                                            //Send using SMTP
-                    $mail->SMTPDebug = SMTP::DEBUG_OFF;                   //Enable verbose debug output
-                    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                    $mail->Username   = $_ENV['SMTP_USERNAME'];               //SMTP username
-                    $mail->Password   =  $_ENV['SMTP_PASS'];                   //SMTP password
-                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-                    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                    $mail->isSMTP();                                            
+                    $mail->SMTPDebug = SMTP::DEBUG_OFF;                   
+                    $mail->Host       = 'smtp.ionos.com';                     
+                    $mail->SMTPAuth   = true;                                   
+                    $mail->Username   = $_ENV['SMTP_USERNAME'];              
+                    $mail->Password   =  $_ENV['SMTP_PASS'];                   
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+                    $mail->Port       = 465;                                    
 
                     // Set email body content from file template
                     $templatePath = ROOT . DS . 'templates' . DS . 'email' . DS . 'html' . DS . 'account_creation.php';
@@ -141,8 +89,8 @@ class UsersController extends AppController
 
                     // Send user email
                     $mail->setFrom('wafulacharles47@gmail.com', 'Masinde Charles');
-                    $mail->addAddress($user->email, $user->name);     //Add a recipient
-                    $mail->isHTML(true);                                  //Set email format to HTML
+                    $mail->addAddress($user->email, $user->name);     
+                    $mail->isHTML(true);                                  
                     $mail->Subject = 'Account Creation';
                     $mail->Body    = $userEmailBodyContent;
                     $mail->AltBody = 'Success!';
@@ -151,19 +99,20 @@ class UsersController extends AppController
                     // Send admin email
                     $mail->clearAddresses();
                     $mail->addAddress('luhyabandit@gmail.com', 'Wafs');
-                    $mail->isHTML(false);                                  //Set email format to plain text
+                    $mail->isHTML(false);                                  
                     $mail->Subject = 'New User Created';
                     $mail->Body    = $adminEmailBodyContent;
                     $mail->AltBody = 'A new user has been created.';
                     $mail->send();
 
-                    $this->Flash->success(__('The user has been saved.'));
                     return $this->redirect(['action' => 'index']);
                 } catch (Exception $e) {
                     $errorMessage = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                     $this->Flash->error(__('The user has been saved but the email could not be sent. Please try again later. Error message: {0}', $errorMessage));
                     return $this->redirect(['action' => 'index']);
                 }
+                $this->Flash->success(__('The user has been saved.'));
+
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
